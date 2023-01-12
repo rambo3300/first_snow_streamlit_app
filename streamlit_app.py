ac54ec1173d4197cec86fc491efb676d719323e0
RITABRATA CHAKRABORTY <chakrabortyritabrata8@gmail.com>
2023-01-12 13:19:39 +0530

    Update streamlit_app.py
    updated header and added some emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocker Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Favourites")
+streamlit.text(":bowl_with_spoon:Omega 3 & Blueberry Oatmeal")
+streamlit.text(":green_salad:Kale, Spinach and Rocker Smoothie")
+streamlit.text(":chicken:Hard-Boiled Free-Range Egg")
+streamlit.text(":avocado::bread: Avocaso Toast")
